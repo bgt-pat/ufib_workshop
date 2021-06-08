c403d6b62588e9ad1606e8ec8d4ffc5ed0f244b4
Ivan Pribec <ivan.pribec@gmail.com>
2021-06-08 13:36:14 +0200

    Fix seconds dataframe

--- notebooks/solutions/time_to_seconds.py
+++ notebooks/solutions/time_to_seconds.py
@@ -1,4 +1,3 @@
 dataset['Time'] = dataset['Time'] - dataset['Time'].iloc[0] # subtract initial time
-dataset['hours'] = dataset['Time'].dt.total_seconds() # append seconds to dataset
-dataset[[r"$T_1$", r"$T_2$", r"$T_3$"]] += 273.15
+dataset['seconds'] = dataset['Time'].dt.total_seconds() # append seconds to dataset
 dataset.head()
